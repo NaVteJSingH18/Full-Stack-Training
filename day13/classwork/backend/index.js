05211364e63c48c20e3aa98d3bfbd42eb42f9f8b
NaVteJSingH18 <navtej.dgc@gmail.com>
2025-07-18 09:24:06 +0530

    new content

--- day13/classwork/backend/index.js
+++ day13/classwork/backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const {connection}=require("./config/db")
 const app = express();
 
 const productRoutes = require("./routes/productroutes");
 const logger = require("./middlewares/logger"); // optional
+// const { connection } = require("mongoose");
 
 app.use(express.json());
 app.use(logger); // optional
@@ -14,6 +16,15 @@ app.get("/", (req, res) => {
 app.use("/products", productRoutes);
 
 const PORT = 8080;
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
+  try{
+    console.log("data base is connecting..")
+    await connection
+    console.log("data base is connected")
+  }catch(error){
+    
+      console.log(error)
+    }
+  
   console.log(`Server is running at http://localhost:${PORT}`);
 });
